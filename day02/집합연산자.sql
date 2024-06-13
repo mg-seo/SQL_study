ba38fe128120609d4d482ae6369f37e6e25b1ef6
mg-seo <mgseokr@gmail.com>
2024-06-13 17:34:04 +0900

    Update 집합연산자.sql

--- day02/집합연산자.sql
+++ day02/집합연산자.sql
@@ -16,3 +16,10 @@ SELECT first_name, hire_date FROM employees WHERE department_id = 20;
 SELECT first_name, hire_date FROM employees WHERE hire_date LIKE '04%'
 MINUS -- 차집합, 교집합 제거
 SELECT first_name, hire_date FROM employees WHERE department_id = 20;
+-----------------------------------------------------------------------
+-- 집합연산자는 DUAL같은 가상 데이터를 만들어서 합칠 수도 있다
+SELECT 200 AS 번호, 'HONG' AS 이름, '서울시' AS 지역 FROM DUAL
+UNION ALL
+SELECT 300, 'LEE', '경기도' FROM DUAL
+UNION ALL
+SELECT EMPLOYEE_ID, LAST_NAME, '서울시' FROM EMPLOYEES;
